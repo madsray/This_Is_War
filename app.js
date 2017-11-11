787d0dfc52e7bf4b4e5eca86c6897eb356f10f89
madsray <30870145+madsray@users.noreply.github.com>
2017-11-11 17:22:56 -0600

    Commit 5 day3 - variables with card images created

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(() =>{
 
 console.log("hey this is working!")
-
+//++++++++++++++++++++++++++++++++++++++++++++++++
 //create deal button to be click able
 //deal button splits cards randomly between computer array and Player array
 //text changes showing amount of players and computers cards in their pile
@@ -14,6 +14,77 @@ console.log("hey this is working!")
 //check to see if player has any remaining Cards
 //if computer doesn't player wins
 //if player doesn't computer wins
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const $2C = $('<img>').attr("src","images/2C.png");
+const $2D = $('<img>').attr("src","images/2D.png");
+const $2H = $('<img>').attr("src","images/2H.png");
+const $2S = $('<img>').attr("src","images/2S.png");
+const $3C = $('<img>').attr("src","images/3C.png");
+const $3D = $('<img>').attr("src","images/3D.png");
+const $3S = $('<img>').attr("src","images/3S.png");
+const $3H = $('<img>').attr("src","images/3H.png");
+const $4C = $('<img>').attr("src","images/4C.png");
+const $4D = $('<img>').attr("src","images/4D.png");
+const $4H = $('<img>').attr("src","images/4H.png");
+const $4S = $('<img>').attr("src","images/4S.png");
+const $5C = $('<img>').attr("src","images/5C.png");
+const $5D = $('<img>').attr("src","images/5D.png");
+const $5S = $('<img>').attr("src","images/5S.png");
+const $5H = $('<img>').attr("src","images/5H.png");
+const $6C = $('<img>').attr("src","images/6C.png");
+const $6D = $('<img>').attr("src","images/6D.png");
+const $6S = $('<img>').attr("src","images/6S.png");
+const $6H = $('<img>').attr("src","images/6H.png");
+const $7C = $('<img>').attr("src","images/7C.png");
+const $7D = $('<img>').attr("src","images/7D.png");
+const $7S = $('<img>').attr("src","images/7S.png");
+const $7H = $('<img>').attr("src","images/7H.png");
+const $8C = $('<img>').attr("src","images/8C.png");
+const $8D = $('<img>').attr("src","images/8D.png");
+const $8S = $('<img>').attr("src","images/8S.png");
+const $8H = $('<img>').attr("src","images/8H.png");
+const $9C = $('<img>').attr("src","images/9C.png");
+const $9D = $('<img>').attr("src","images/9D.png");
+const $9S = $('<img>').attr("src","images/9S.png");
+const $9H = $('<img>').attr("src","images/9H.png");
+const $10C = $('<img>').attr("src","images/10C.png");
+const $10D = $('<img>').attr("src","images/10D.png");
+const $10S = $('<img>').attr("src","images/10S.png");
+const $10H = $('<img>').attr("src","images/10H.png");
+const $JH = $('<img>').attr("src","images/JH.png");
+const $JS = $('<img>').attr("src","images/JS.png");
+const $JD = $('<img>').attr("src","images/JD.png");
+const $JC = $('<img>').attr("src","images/JC.png");
+const $QS = $('<img>').attr("src","images/QS.png");
+const $QD = $('<img>').attr("src","images/QD.png");
+const $QC = $('<img>').attr("src","images/QC.png");
+const $QH = $('<img>').attr("src","images/QH.png");
+const $KD = $('<img>').attr("src","images/KD.png");
+const $KS = $('<img>').attr("src","images/KS.png");
+const $KH = $('<img>').attr("src","images/KH.png");
+const $KC = $('<img>').attr("src","images/KC.png");
+const $AC = $('<img>').attr("src","images/AC.png");
+const $AS = $('<img>').attr("src","images/AS.png");
+const $AH = $('<img>').attr("src","images/AH.png");
+const $AD = $('<img>').attr("src","images/AD.png");
+const cardArray = [
+  {
+    imageAssign: $2C,
+    value: 2,
+    name: "two"
+  },
+  {
+    value:2
+  }]
 
 
-})
+
+
+
+
+
+
+
+
+
+});

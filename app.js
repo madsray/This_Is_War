57b81c407280513451f429fe9e2ad4f3868bfbd0
madsray <30870145+madsray@users.noreply.github.com>
2017-11-11 11:02:29 -0600

    Commit 2 day 3 - pseudocode added

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ $(() =>{
 
 console.log("hey this is working!")
 
-
+//create deal button to be click able
+//deal button splits cards randomly between computer array and Player array
+//text changes showing amount of players and computers cards in their pile
+//create player card as button
+//when player clicks card flip over both Cards
+//if computers card is greater push into computers array
+//
 
 
 })

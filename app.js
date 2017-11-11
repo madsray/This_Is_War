5b40b96fe6ef752f2e341fcd46a995897cf4ae6d
madsray <30870145+madsray@users.noreply.github.com>
2017-11-11 16:11:43 -0600

    Commit 4 - day 3 - pseudocode finished

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ console.log("hey this is working!")
 //text changes showing amount of players and computers cards in their pile
 //create player card as button
 //when player clicks card flip over both Cards
-//if computers card is greater push into computers array
-//
+//if computers card is greater push cards into computers array
+//if players card is greater push cards into players array
+//if cards are tied. flip over another card
+//check to see if computer has any remaining cards
+//check to see if player has any remaining Cards
+//if computer doesn't player wins
+//if player doesn't computer wins
 
 
 })
